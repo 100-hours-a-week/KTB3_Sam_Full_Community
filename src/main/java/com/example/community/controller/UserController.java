a0dc937125032ee6c9a2dcb87f1754b21c097f99
sam <simonana@naver.com>
2025-10-21 13:45:19 +0900

    feat: 유저 컨트롤러 Tag 추가

--- src/main/java/com/example/community/controller/UserController.java
+++ src/main/java/com/example/community/controller/UserController.java
@@ -14,11 +14,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "유저 API", description = "유저 정보 관련 API")
 @RestController
 public class UserController {
     private final UserService userService;
