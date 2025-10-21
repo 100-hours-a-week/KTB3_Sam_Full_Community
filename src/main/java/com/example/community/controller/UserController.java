0bff47a400a226b80870bf1bb16e53fd9ef60225
sam <simonana@naver.com>
2025-10-21 09:51:45 +0900

    feat: 회원정보 수정 API 문서화

--- src/main/java/com/example/community/controller/UserController.java
+++ src/main/java/com/example/community/controller/UserController.java
@@ -13,6 +13,7 @@ import com.example.community.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -40,14 +41,29 @@ public class UserController {
         return ResponseEntity.ok(APIResponse.success(SuccessCode.USER_REGISTERED, UserRegisterResponse.from(user)));
     }
 
+    @Operation(summary = "회원 상세 정보 조회", description = "로그인된 회원의 상세 정보를 조회합니다.")
     @GetMapping("/users")
+    @SecurityRequirement(name = "JWT")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "user_find_success"),
+            @ApiResponse(responseCode = "400", description = "invalid_request"),
+            @ApiResponse(responseCode = "500", description = "internal_server_error")
+    })
     public ResponseEntity<APIResponse<UserInfoResponse>> getUser(HttpServletRequest servletRequest) {
         Long userId = jwtUtil.extractUserId((String) servletRequest.getAttribute("accessToken"));
         User user = userService.getUser(userId);
         return ResponseEntity.ok(APIResponse.success(SuccessCode.USER_FOUND,UserInfoResponse.from(user)));
     }
 
+    @Operation(summary = "회원 정보 수정", description = "로그인된 회원의 정보를 수정합니다.")
     @PatchMapping("/users")
+    @SecurityRequirement(name = "JWT")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "user_info_update_success"),
+            @ApiResponse(responseCode = "400", description = "invalid_request"),
+            @ApiResponse(responseCode = "401", description = "not_login_user"),
+            @ApiResponse(responseCode = "500", description = "internal_server_error")
+    })
     public ResponseEntity<APIResponse<Void>> modifyUser(HttpServletRequest servletRequest, @Valid @RequestBody UserModifyRequest request) {
         Long userId = jwtUtil.extractUserId((String) servletRequest.getAttribute("accessToken"));
         userService.modifyUser(userId, request.nickname(), request.profileImageId());
