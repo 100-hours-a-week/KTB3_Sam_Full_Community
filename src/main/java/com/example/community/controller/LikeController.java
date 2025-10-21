697dbb953bc7aa39d6b146b28f0cee0125d18a45
sam <simonana@naver.com>
2025-10-21 14:32:34 +0900

    feat: 좋아요 추가 API 문서화

--- src/main/java/com/example/community/controller/LikeController.java
+++ src/main/java/com/example/community/controller/LikeController.java
@@ -6,6 +6,12 @@ import com.example.community.dto.response.APIResponse;
 import com.example.community.dto.response.LikePostResponse;
 import com.example.community.entity.Like;
 import com.example.community.service.LikeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "좋아요 API", description = "좋아요 추가,삭제 API")
 @RestController
 public class LikeController {
     private final LikeService likeService;
@@ -23,8 +30,18 @@ public class LikeController {
         this.jwtUtil = jwtUtil;
     }
 
+    @Operation(summary = "좋아요 추가", description = "입력받은 유저 ID와 게시글 ID에 해당하는 좋아요를 생성합니다.")
     @PostMapping("/boards/{boardId}/like")
-    public ResponseEntity<APIResponse<LikePostResponse>> postLike(HttpServletRequest servletRequest, @PathVariable("boardId") Long boardId) {
+    @SecurityRequirement(name = "JWT")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "board_like_success"),
+            @ApiResponse(responseCode = "404", description = "not_found_board"),
+            @ApiResponse(responseCode = "400", description = "invalid_request"),
+            @ApiResponse(responseCode = "500", description = "internal_server_error")
+    })
+    public ResponseEntity<APIResponse<LikePostResponse>> postLike(HttpServletRequest servletRequest,
+                                                                  @Parameter(description = "좋아요 추가할 게시글 ID", required = true, example = "3")
+                                                                  @PathVariable("boardId") Long boardId) {
         Long userId = jwtUtil.extractUserId((String) servletRequest.getAttribute("accessToken"));
         Like like = likeService.postLike(userId, boardId);
         return ResponseEntity.ok(APIResponse.success(SuccessCode.BOARD_LIKED, LikePostResponse.from(like)));
