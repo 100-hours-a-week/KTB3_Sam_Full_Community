42289e037fab7f7245b27c4ee00850062e8d4f17
sam <simonana@naver.com>
2025-11-03 08:46:53 +0900

    feat: LikeRepository JPA로 구현

--- src/main/java/com/example/community/repository/LikeRepository.java
+++ src/main/java/com/example/community/repository/LikeRepository.java
@@ -1,8 +1,13 @@
 package com.example.community.repository;
 
+import com.example.community.entity.Board;
 import com.example.community.entity.Like;
+import com.example.community.entity.User;
 import com.example.community.repository.interfaces.LikeCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikeRepository extends JpaRepository<Like, Long>, LikeCustomRepository {
+    public Optional<Like> findByUserAndPost(User user, Board post);
 }
