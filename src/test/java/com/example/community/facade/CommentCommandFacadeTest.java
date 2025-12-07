c66aab010a50e7aae529e54669c0bc10313eb12a
sam <simonana@naver.com>
2025-12-07 20:54:38 +0900

    feat: CommentCommandFacade 테스트 코드 구현

---
+++ src/test/java/com/example/community/facade/CommentCommandFacadeTest.java
@@ -0,0 +1,65 @@
+package com.example.community.facade;
+
+import com.example.community.entity.Board;
+import com.example.community.entity.Comment;
+import com.example.community.entity.User;
+import com.example.community.service.BoardService;
+import com.example.community.service.CommentService;
+import com.example.community.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class CommentCommandFacadeTest {
+    @Mock
+    CommentService commentService;
+
+    @Mock
+    UserService userService;
+
+    @Mock
+    BoardService boardService;
+
+    @InjectMocks
+    CommentCommandFacade commentCommandFacade;
+
+    @Test
+    void 입력받은_게시글과_유저에대해_댓글을_생성한다() {
+        //given
+        Long userId = 1L;
+        Long boardId = 10L;
+        String content = "hello comment";
+
+        User user = new User("email", "password", "nickname");
+        ReflectionTestUtils.setField(user, "id", userId);
+
+        Board board = new Board("title", "content", user);
+        ReflectionTestUtils.setField(board, "id", boardId);
+
+        Comment expectedComment = new Comment(user, board, content);
+
+        given(userService.getUser(userId)).willReturn(user);
+        given(boardService.findById(boardId)).willReturn(board);
+        given(commentService.save(user, board, content)).willReturn(expectedComment);
+
+
+        //when
+        Comment result = commentCommandFacade.post(userId, boardId, content);
+
+
+        //then
+        assertThat(result.getContent()).isEqualTo(content);
+        assertThat(result.getUser().getId()).isEqualTo(userId);
+        assertThat(result.getBoard().getId()).isEqualTo(boardId);
+    }
+}
